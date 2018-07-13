4fd90f63573a9f39c2504ddced361d6792a07df8
“Goodvalley-Hotmail” <goodvalley@hotmail.com>
2018-07-13 19:10:36 +0200

    v001

--- src/meta-box.php
+++ src/meta-box.php
@@ -13,6 +13,19 @@ namespace KnowTheCode\MetaBoxBasics;
 
 use WP_Post;
 
+/*
+ * CARLES START
+ * As soon as the admin_menu Event is fired, we call a register callback function register_meta_box(),
+ * which will register a MetaBox using a WordPress core function called add_meta_box(), located at
+ * /wp-admin/includes/template.php, around line 917.
+ *
+ * In the Comments section of that WordPress function, it says that the function needs an id, a title, a callback
+ * for rendering the desired Content and the screen we want to put it on. The screen and the rest of the parameters
+ * is optional.
+ *
+ *
+ * CARLES END
+ */
 add_action( 'admin_menu', __NAMESPACE__ . '\register_meta_box' );
 /**
  * Register the meta box.
@@ -23,10 +36,10 @@ add_action( 'admin_menu', __NAMESPACE__ . '\register_meta_box' );
  */
 function register_meta_box() {
 	add_meta_box(
-		'mbbasics_subtitle',
-		__( 'Subtitle', 'mbbasics' ),
-		__NAMESPACE__ . '\render_meta_box',
-		'post'
+		'mbbasics_subtitle', // CARLES -> This is our id.
+		__( 'Subtitle', 'mbbasics' ), // CARLES -> This is our title. It is wrapped in a function that allows it to be translated.
+		__NAMESPACE__ . '\render_meta_box', // CARLES -> When it's time to render, WordPress will call this function. Now it is in line 56.
+		'post' // CARLES -> We want to render in the 'post' screen. If we want it also in a Page or CPT, we could build an Array.
 	);
 }
 
@@ -50,7 +63,7 @@ function render_meta_box( WP_Post $post, array $meta_box ) {
 
 	// Load the view file
 	include METABOXBASICS_DIR . 'src/view.php';
-}
+}git push -u origin master
 
 add_action( 'save_post', __NAMESPACE__ . '\save_meta_box', 10, 2 );
 /**
